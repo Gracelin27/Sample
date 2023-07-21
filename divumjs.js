249a9936caec19c9c3dba353eceeca4ca7b6c52b
Gracelin27 <jonax.gracelin@divum.in>
2023-07-21 17:48:11 +0530

    js1

--- divumjs.js
+++ divumjs.js
@@ -1,15 +1,38 @@
 function company(){
-    document.getElementById("sub1").classList.toggle("show");
+  document.getElementById("sub1").classList.toggle("show");
+  // document.getElementById("sub1").style.display="block";
 }
 window.onclick=function(event){
-    if (!event.target.matches('.dropdown')) {
-        var dropdowns = document.getElementsByClassName("sub");
-        var i;
-        for (i = 0; i < dropdowns.length; i++) {
-          var openDropdown = dropdowns[i];
-          if (openDropdown.classList.contains('show')) {
-            openDropdown.classList.remove('show');
-          }
+  if (!event.target.matches('.dropdown')) {
+      var dropdowns = document.getElementsByClassName("sub");
+      var i;
+      for (i = 0; i < dropdowns.length; i++) {
+        var openDropdown = dropdowns[i];
+        if (openDropdown.classList.contains('show')) {
+          openDropdown.classList.remove('show');
         }
       }
     }
+  }
+  function change(evt, pic) {
+    // Declare all variables
+    var i, tabcontent, tablinks;
+  
+    // Get all elements with class="tabcontent" and hide them
+    tabcontent = document.getElementsByClassName("con");
+    for (i = 0; i < tabcontent.length; i++) {
+      tabcontent[i].style.display = "none";
+    }
+  
+    // Get all elements with class="tablinks" and remove the class "active"
+    tablinks = document.getElementsByClassName("links");
+    for (i = 0; i < tablinks.length; i++) {
+      tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+  
+    // Show the current tab, and add an "active" class to the button that opened the tab
+    document.getElementById(pic).style.display = "block";
+    evt.currentTarget.className += " active";
+  }
+
+  document.getElementById("defaultOpen").click();
